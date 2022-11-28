3597945827d3cc5f812ee3a703a1425f73bbed3f
arifulgony <arifulgony@gmail.com>
2022-11-28 20:25:57 +0600

    dashboard

--- src/Pages/Dashboard/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Dashboard = () => {
     return (
         <div>
-            <h2>Coming Son</h2>
+            {/* <h2>Coming Son</h2> */}
         </div>
     );
 };
